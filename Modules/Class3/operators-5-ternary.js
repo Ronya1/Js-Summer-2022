685700dc14bf6dc72cbd9b6146055ef20adef393
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-06-30 10:18:25 -0400

    updated work

---
+++ Modules/Class3/operators-5-ternary.js
@@ -0,0 +1,63 @@
+/**
+ * 5. Ternary (?:)
+ *     Used in situations where one of two values will be assigned to variables depending upon the result of condition(s)
+ *     When you have more then 2 values you cannot use Ternary - unlimited oncditions but value can only be 2 
+ * 
+ * 4.5 , 5.5 or 6 
+ * 
+ * if credit score is > 700, rate will be 4.5 else 5.5 - 1 Value 
+ * 
+ * if credit score is > 700 and first time home buyer, rate will be 4.5 else 5.5 - 2 values 
+ * 
+ * if credit score > 700 or first time home buyer , rate will 4.5 else 5.5
+ * 
+ * 
+ * syntax:
+ * varName = condition(s) ? value-WhichWillBeAssignIfConditionResultIntoTrue : value-WhichWillBeAssignIfConditionResultIntoFalse
+ */
+
+// if credit core > 700, rate will be 4.5 else 5.5 
+
+let userCreditScore = 700;
+let mortgageRate = userCreditScore > 700 ? 4.5 : 5.5
+
+console.log(`/nCredit Score .-> ${userCreditScore}`)
+console.log(`Mortgage Rate .-> ${mortgageRate}`)
+
+// if credit score > 700 and first time home buyer, rate will 4.5 else 5.5
+let userCreditScore2 = 710;
+let isFirstTimeBuyer = true;
+
+let mortgageRate2 = userCreditScore2 > 700 && isFirstTimeBuyer === true ? 4.5 : 5.5;
+// let mortgageRate2 = userCreditScore2 > 700 && isFirstTimeBuyer ? 4.5 : 5.5;
+console.log(`/nCredit Score .-> ${userCreditScore2}`)
+console.log(`Credit Score .-> ${isFirstTimeBuyer}`)
+console.log(`Mortgage Rate .-> ${mortgageRate2}`)
+
+/*
+    isFirstTimeBuyer = true
+
+    isFirstTimeBuyer === true   // isFirstTimeBuyer
+    isFirstTimeBuyer
+    true
+
+
+    isFirstTimeBuyer === false  // !isFirstTimeBuyer
+    true === false
+    false
+*/
+
+
+/*
+    isFirstTimeBuyer = false
+
+    isFirstTimeBuyer === true   // isFirstTimeBuyer
+    false === true
+    false
+
+
+    isFirstTimeBuyer === false  // !isFirstTimeBuyer
+    false === false
+    true
+   
+*/
---
+++ Modules/Class3/operators-6-Advanced_Assignments.js
@@ -0,0 +1,46 @@
+/**
+ * 6. Advanced Assignment (+=,-=,*=,/=,%=)
+ *      
+ * 
+ */
+let num1 = 5;
+// ue increment operator to ad 5 in num 1 
+//num1++;
+//num1++;
+//num1++;
+//num1++;
+//num1++;
+// num1++;
+
+num1 = num1 + 5 // can write this like num1=+5
+console.log(`num1 -> ${num1}`); // 10
+
+num1 -= 2;   // num1 = num1 - 2;  // 8
+console.log(`num1 -> ${num1}`);
+
+num1 *= 2;   // num1 = num1 - 2;  // 16 
+console.log(`num1 -> ${num1}`);
+
+num1 /= 3;   // num1 = num1 - 2;  // 5.33
+console.log(`num1 -> ${num1}`);
+
+num1 = 4.33
+num1 %= 5;   // num1 = num1 - 2;  // 4.33 
+console.log(`num1 -> ${num1}`);
+
+/**
+ * Banking 
+ * 
+ * currentBalance = 1000
+ * 
+ * deposit = 200
+ * 
+ * currentBalance = currentBalance + deposit        // currentBalance += Deposit
+ * 
+ * withdraw = 50
+ * 
+ * currentBalance = currentBalance - withdraw       // currentBalance -= withdraw
+ * 
+ */
+
+
