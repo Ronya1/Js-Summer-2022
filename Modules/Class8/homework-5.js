c6c735db8f281134c054a8909a16327cb29895ad
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-07-14 21:01:23 -0400

    hw5

--- Modules/Class8/homework-5.js
+++ Modules/Class8/homework-5.js
@@ -10,6 +10,23 @@
  * 'apple banana cherry' -> 'Apple Banana Cherry'
  * 
  */
+/**
+ * HOW WOULD YOU SOLVE THIS
+ * 1. create a variable for the sentence 
+ * 2. convert variable into an array 
+ * 3. capitalize the 0 index 
+ */
+// NO idea how I figured this out -> trial and error 
+let sent1 = 'have a great day'.split(' ')
+let sent1_TC = '';
+
+for (i = 0 ; i <= sent1.length-1 ; i++){
+    sent1_TC = sent1_TC + ' ' + sent1[i].substring(0,1).toUpperCase() + sent1[i].substring(1).toLowerCase();
+}
+console.log(sent1_TC)
+
+
+
 
 /**
  * Q2:
@@ -21,6 +38,19 @@
  * 'Learn' -> 'Learn'
  * 
  */
+/**
+ * HOW TO SOLVE THIS?
+ * create an array
+ * 
+ */
+
+let sent2 = 'have a great day'.split(' ')
+let reversed_sent2 = '';
+
+for (i = sent2.length-1 ; i >= 0 ; i--) {
+    reversed_sent2 = reversed_sent2 + ' ' + sent2[i];
+}
+console.log(reversed_sent2)
 
 /**
  * Q3:
@@ -30,6 +60,14 @@
  * [1, 1, 1, 2, 3, 1, 2] -> 11
  * [1, -1] -> 0
  */
+let myNumber = [1, 2, 3, 4, 5]
+let storeOfVal = 0;
+for (i = 0 ; i <= myNumber.length-1 ; i++) {
+    storeOfVal = storeOfVal + myNumber[i];
+}
+
+console.log(storeOfVal)
+
 
 /**
  * Q4:
@@ -41,3 +79,10 @@
  * [1, 1, 1, 2, 3, 1, 2] -> 11/7 -> 1.xx
  * [1, -1] -> 0/2 -> 0
  */
+let myAverage = [1, 2, 3, 4, 5] // 15/5 = 3
+let myAvg = 0;
+
+for (i = 0 ; i <= myAverage.length-1 ; i++) {
+    myAvg = myAvg + myAverage[i] / 5; 
+}
+console.log(myAvg)
