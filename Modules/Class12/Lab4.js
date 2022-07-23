5b80e4498f5b7817d4342b37da21ea2926d4e1bd
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-07-23 11:07:56 -0400

    new

---
+++ Modules/Class12/Lab4.js
@@ -0,0 +1,17 @@
+let human1 = [
+    {name1 : 'Frank', age : 27, true : true, 1 : 'one'},
+    {name1 : 'Joe', age : 40, true : false, 1 : 'two'},
+    {name1 : 'Tom', age : 19, true : true, 1 : 'one'},
+];
+
+const myHuman = human1.find(car => car.name1 === 'Joe');
+console.log(myHuman)
+
+
+/**
+ * let funcName = (<inputParameters>) => {
+ *      code inside 
+ *      the function
+ *      return statement  --> ONLY IF you plan to return something back to user at the end of function (Q3)
+ */
+
