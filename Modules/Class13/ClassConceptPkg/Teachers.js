ece4d043dc0651ce1626dc5bae69adfbfd5aab2f
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-07-25 22:09:11 -0400

    new notes

---
+++ Modules/Class13/ClassConceptPkg/Teachers.js
@@ -0,0 +1,51 @@
+class Teachers {
+
+    static teacherCount = 0;
+
+    teacher = {
+        tName : '',
+        tAge : 0,
+        tLocation : '',
+        tId : 0,
+        tCourseName : '',
+        tSalary : 0
+    }
+
+    /**
+     * 
+     * age >= 21
+     * 
+     * teacherCourse should be Automation, QA or Mobile
+     * 
+     */
+    joining(teacherName, teacherAge, teacherCountry, teacherCourseName) {
+        // 
+    }
+
+    showMyDetails() {
+        // code
+    }
+
+    changeTeachingCourse() {
+        // code
+    }
+
+
+    submitDocuments() {
+        // code
+    }
+
+    orderBooks() {
+        // code
+    }
+
+    addGrade() {
+        // code
+    }
+
+    removeGrade() {
+        // code
+    }
+
+
+}
